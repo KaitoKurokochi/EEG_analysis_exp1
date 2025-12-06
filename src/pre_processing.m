e4e89e9e53c1a4988503e1f0d0211fef141f2842
KaitoKurokochi <kaito.24.kurokochi.09@gmail.com>
2025-12-06 15:51:35 +0900

    :+1: visualize TFR info

--- src/pre_processing.m
+++ src/pre_processing.m
@@ -61,8 +61,39 @@ ft_databrowser(cfg, comp);
 
 %% remove noisy comps 
 cfg = [];
-cfg.component = [1 2 3 13 14 15 9 7 8]; % to be removed component(s)
+cfg.component = [1, 2, 3, 4, 8, 14]; % to be removed component(s)
 data = ft_rejectcomponent(cfg, comp, data);
+
+%% TFR analysis 
+% freq analysis
+cfg              = [];
+cfg.output       = 'pow'; % output = power 
+cfg.channel      = 'all';
+cfg.method       = 'mtmconvol';
+cfg.taper        = 'hanning';
+cfg.foi          = 2:2:30;                         % analysis 2 to 30 Hz in steps of 2 Hz
+cfg.t_ftimwin    = ones(length(cfg.foi),1).*0.5;   % length of time window = 0.5 sec
+cfg.toi          = -1.0:0.05:1.5;                      % the time window "slides" from -0.5 to 1.5 in 0.05 sec steps
+
+spectr = ft_freqanalysis(cfg, data); 
+
+%% plot [freq x power] graph
+channel_indices = ~cellfun(@(x) strcmp(x, 'EOG'), spectr.label);
+pow_to_plot = nanmean(spectr.powspctrm(channel_indices, :, :), 3);
+
+figure;
+plot(spectr.freq, pow_to_plot');
+xlabel('Frequency (Hz)');
+ylabel('absolute power (uV^2)');
+
+%% figure [time x freq] graph for all channel
+cfg = [];
+cfg.baseline     = [-0.2 -0.0];
+cfg.channel      = 'all';
+cfg.baselinetype = 'absolute';
+cfg.showlabels   = 'yes';
+cfg.layout       = 'easycapM11.mat';
+figure; ft_multiplotTFR(cfg, spectr);
 
 %% data saving
 save(v1_path, 'data', '-v7.3');
