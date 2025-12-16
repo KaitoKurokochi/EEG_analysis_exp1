ab705f2fbd59ff2325783f80670d46b95a821232
KaitoKurokochi <kaito.24.kurokochi.09@gmail.com>
2025-12-16 10:43:50 +0900

    :+1: double ICA

--- src/main_1.m
+++ src/main_1.m
@@ -1,16 +1,19 @@
 % main_1: segment-base pre-processing
-% -> v1 (pre-processed)
+% -> v1 (pre-processed, saved in result/v1)
 
 %% define path
 set_path;
 groups = {'nov', 'exp'};
 
 for g = 1:length(groups)
-    for i = 1:5
+    for i = 1:12
         pname = [groups{g}, num2str(i)];
         data_dir = fullfile(prj_dir, 'rawdata', pname);
-        res_dir = fullfile(prj_dir, 'result', pname);
         vhdrs = dir(fullfile(data_dir, '*.vhdr'));
+        res_dir = fullfile(prj_dir, 'result', 'v1', pname);
+        if ~exist(res_dir, 'dir')
+            mkdir(res_dir);
+        end
         
         for v = 1:length(vhdrs)
             vhdr_path = fullfile(data_dir, vhdrs(v).name);
@@ -19,18 +22,26 @@ for g = 1:length(groups)
 
             disp(['--- id: ', id, ', start pre-processing ---']);
             
-            % ica 
-            [data, cleaned_data, ica_result] = pre_processing(vhdr_path, sequence_path, id);
+            % pre-processing
+            if 1 <= i && i <= 5
+                [data0, data1, data1_ica2, ic1, ic1_ica2] = pre_processing(vhdr_path, sequence_path, id, 'mytrialfun_2');
+            end
+            if 6 <= i && i <= 12
+                [data0, data1, data1_ica2, ic1, ic1_ica2] = pre_processing(vhdr_path, sequence_path, id, 'mytrialfun');
+            end
 
             % spectrum 
-            spectr0 = my_calc_spectr(data);
-            spectr1 = my_calc_spectr(cleaned_data);
+            spectr0 = my_calc_spectr(data0);
+            spectr1 = my_calc_spectr(data1);
+            spectr1_ica2 = my_calc_spectr(data1_ica2);
 
             % save 
-            save(fullfile(res_dir, ['v0_', num2str(v), '_ica2.mat']), 'data', '-v7.3');
-            save(fullfile(res_dir, ['v1_', num2str(v), '_ica2.mat']), 'cleaned_data', '-v7.3');
-            save(fullfile(res_dir, ['v0_', num2str(v), '_spectrum_ica2.mat']), 'spectr0', '-v7.3');
-            save(fullfile(res_dir, ['v1_', num2str(v), '_spectrum_ica2.mat']), 'spectr1', '-v7.3');           
+            save(fullfile(res_dir, ['v0_', num2str(v), '.mlat']), 'data0', '-v7.3');
+            save(fullfile(res_dir, ['v1_', num2str(v), '.mat']), 'data1', '-v7.3');
+            save(fullfile(res_dir, ['v1_', num2str(v), '_ica2.mat']), 'data1_ica2', '-v7.3');
+            save(fullfile(res_dir, ['v0_', num2str(v), '_spectrum.mat']), 'spectr0', '-v7.3');
+            save(fullfile(res_dir, ['v1_', num2str(v), '_spectrum.mat']), 'spectr1', '-v7.3');
+            save(fullfile(res_dir, ['v1_', num2str(v), '_spectrum_ica2.mat']), 'spectr1_ica2', '-v7.3');
         end
     end
 end
