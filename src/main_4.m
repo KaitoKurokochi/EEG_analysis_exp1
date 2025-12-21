721e76fc6f4d7e4155350284723db471900c7df8
KaitoKurokochi <kaito.24.kurokochi.09@gmail.com>
2025-12-21 12:36:41 +0900

    :+1: update main_4

--- src/main_4.m
+++ src/main_4.m
@@ -2,3 +2,20 @@
 % read data as below
 % - result/v3/{group}.mat: v3 of {group} (include data_v3)
 
+set_path;
+num_type = 4;
+
+data_dir = fullfile(prj_dir, 'result', 'v3');
+res_dir = fullfile(prj_dir, 'result', 'v4');
+if ~exist(res_dir, 'dir')
+    mkdir(res_dir);
+end
+
+% read data_v3
+load(data_dir, 'nov.mat');
+data_nov = data_v3;
+load(data_dir, 'exp.mat');
+data_exp = data_v3;
+
+%% phase opposition 
+main_channels = {'Cz', 'Pz', 'Fz'};
