15366e8539cac13320ded96104011a3bf234a583
KaitoKurokochi <kaito.24.kurokochi.09@gmail.com>
2025-12-25 13:16:10 +0900

    update main_1_2: manual noise channel removal

--- src/main_1_2.m
+++ src/main_1_2.m
@@ -1,5 +1,5 @@
-% main_1_2: segment-base pre-processing (rm noise channels, interpolate
-% removed channels, baseline-correlation)
+% main_1_2: segment-base pre-processing (manually remove noise channels and
+% trials)
 % read data as below 
 % - result/v1_1/{pname}_{i}.mat (include data_v1_1) 
 % saves data as below
@@ -15,26 +15,17 @@ end
 
 data_fnames = {dir(fullfile(data_dir, '*.mat')).name};
 
-%%
-for i = 1:length(data_fnames)
+for i = 1:1%length(data_fnames)
     load(fullfile(data_dir, data_fnames{i})); % include data_v1_1
 
     id = erase(data_fnames{i}, '.mat');
     disp(['--- id: ', id, ', start processing ---']);
 
-    % remove noise channel
-    % cfg          = [];
-    % cfg.method   = 'summary';
-    % data_noise_channel_removed = ft_rejectvisual(cfg, data_v1_1);
+    % remove noise channel manually
+    cfg          = [];
+    cfg.method   = 'summary';
+    data_v1_2 = ft_rejectvisual(cfg, data_v1_1);
 
-    % interpolate removed channels
-
-    % baseline correlation 
-    cfg = [];
-    cfg.demean           = 'yes';
-        cfg.baselinewindow   = [-0.2 0];
-    data_v1_2 = ft_preprocessing(cfg, data_v1_1);
-    
     % save 
     save(fullfile(res_dir, [id, '.mat']), 'data_v1_2', '-v7.3');
 end
