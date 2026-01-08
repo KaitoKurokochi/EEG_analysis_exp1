63886534f62e516b3d1865ea0f4829bc1fb47b9c
KaitoKurokochi <kaito.24.kurokochi.09@gmail.com>
2026-01-08 10:16:04 +0900

    Update main_1_2: manual noise channel removal

--- src/main_1_2.m
+++ src/main_1_2.m
@@ -13,25 +13,77 @@ if ~exist(res_dir, 'dir')
     mkdir(res_dir);
 end
 
-data_fnames = {dir(fullfile(data_dir, '*.mat')).name};
+for i = 1:length(groups)
+    for j = 1:12
+        for k = 1:5
+            id = [groups{i}, num2str(j), '-', num2str(k)];
 
-for i = 1:length(data_fnames)
-    load(fullfile(data_dir, data_fnames{i})); % include data_v1_1
+            fname = fullfile(data_dir, [id, '.mat']);
+            if ~exist(fname, 'file')
+                continue;
+            end
 
-    id = erase(data_fnames{i}, '.mat');
-    disp(['--- id: ', id, ', start processing ---']);
-
-    % visual inspection 
-    cfg = [];
-    cfg.viewmode = 'vertical';
-    ft_databrowser(cfg, data_v1_1);
-
-    % remove noise channel manually
-    cfg          = [];
-    cfg.method   = 'summary';
-    cfg.metric  = 'max';
-    data_v1_2 = ft_rejectvisual(cfg, data_v1_1);
-
-    % save 
-    save(fullfile(res_dir, [id, '.mat']), 'data_v1_2', '-v7.3');
+            disp('loading...');
+            load(fname); % include data_v1_1
+            
+            disp(['--- id: ', id, ', start processing ---']);
+            
+            % visual inspection v1_1
+            % cfg = [];
+            % cfg.viewmode = 'vertical';
+            % ft_databrowser(cfg, data_v1_1);
+            
+            % calc freq
+            cfg              = [];
+            cfg.output       = 'pow';
+            cfg.channel      = 'all';
+            cfg.method       = 'mtmfft';
+            cfg.taper        = 'hanning';
+            cfg.foi          = 1:0.5:30; % start:step:end
+            cfg.t_ftimwin    = ones(length(cfg.foi),1).*0.5;  % length of time window = 0.5 sec
+            cfg.toi          = -1.0:0.025:1.5; 
+            cfg.keeptrials   = 'yes';
+            freq = ft_freqanalysis(cfg, data_v1_1);
+            
+            % fig: time x frequency power
+            hFig = my_spectr_power_plot(freq);
+            saveas(hFig, fullfile(res_dir, [id, '_power_before.jpg']));
+            close(hFig);
+            
+            % fig: frequency band topo map
+            hFig = my_freq_band_topomap(freq);
+            saveas(hFig, fullfile(res_dir, [id, '_topo_before.jpg']));
+            close(hFig);
+            
+            % remove noise channel manually
+            cfg          = [];
+            cfg.method   = 'summary';
+            data_v1_2 = ft_rejectvisual(cfg, data_v1_1);
+            
+            % check data after noise channel removal 
+            cfg              = [];
+            cfg.output       = 'pow';
+            cfg.channel      = 'all';
+            cfg.method       = 'mtmfft';
+            cfg.taper        = 'hanning';
+            cfg.foi          = 1:0.5:30; % start:step:end
+            cfg.t_ftimwin    = ones(length(cfg.foi),1).*0.5;  % length of time window = 0.5 sec
+            cfg.toi          = -1.0:0.025:1.5; 
+            cfg.keeptrials   = 'yes';
+            freq = ft_freqanalysis(cfg, data_v1_2);
+            
+            % fig: time x frequency power
+            hFig = my_spectr_power_plot(freq);
+            saveas(hFig, fullfile(res_dir, [id, '_power_after.jpg']));
+            close(hFig);
+            
+            % fig: frequency band topo map
+            hFig = my_freq_band_topomap(freq);
+            saveas(hFig, fullfile(res_dir, [id, '_topo_after.jpg']));
+            close(hFig);
+            
+            % save 
+            save(fullfile(res_dir, [id, '.mat']), 'data_v1_2', '-v7.3');
+        end
+    end
 end
--- src/utils/my_spectr_power_plot.m
+++ src/utils/my_spectr_power_plot.m
@@ -5,7 +5,6 @@ function [fig] = my_spectr_power_plot(spectr)
 
     % plot [freq x power] graph
     channel_indices = ~cellfun(@(x) strcmp(x, 'EOG'), spectr.label);
-    labels_to_plot = spectr.label(channel_indices);
 
     avg_pow = squeeze(mean(mean(spectr.powspctrm, 1, 'omitnan'), 4, 'omitnan'));
     pow_to_plot = avg_pow(channel_indices, :);
@@ -14,6 +13,5 @@ function [fig] = my_spectr_power_plot(spectr)
     plot(spectr.freq, pow_to_plot');
     xlabel('Frequency (Hz)');
     ylabel('absolute power (uV^2)');
-    legend(labels_to_plot, 'Location', 'northeastoutside', 'Interpreter', 'none', 'NumColumns', 3);
 end
 
