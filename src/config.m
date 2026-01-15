7b45018aef431d16ad017f2a4f3cb47f184c809d
KaitoKurokochi <kaito.24.kurokochi.09@gmail.com>
2026-01-15 18:18:34 +0900

    Update: not to track config.m

--- src/config.m
+++
@@ -1,6 +0,0 @@
-prj_dir = 'C:\Users\kaito\workspace\2025_exp1\EEG_analysis_exp1';
-addpath("utils\");
-num_type = 4;
-main_channels = {'Cz', 'Fz', 'Pz'};
-groups = {'nov', 'exp'};
-conditions = {'ff', 'fs', 'sf', 'ss'};
