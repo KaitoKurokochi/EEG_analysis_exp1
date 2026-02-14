51e0f4f313a5b5b5acde55029d6ea9e4499e4195
KaitoKurokochi <kaito.24.kurokochi.09@gmail.com>
2026-02-14 16:30:21 +0900

    Update: add go/nogo

--- src/classification_by_label.m
+++ src/classification_by_label.m
@@ -1,4 +1,5 @@
 % classification_by_label: classify data by label(condtions)
+% collect correct trials and concat each group
 
 config;
 
@@ -8,6 +9,7 @@ if ~exist(res_dir, 'dir')
     mkdir(res_dir);
 end
 
+% conditions
 for gi = 1:length(groups) % group index
     for ci = 1:num_type % condition index
         data_all = [];
@@ -38,12 +40,53 @@ for gi = 1:length(groups) % group index
                     cfg = [];
                     data_all = ft_appenddata(cfg, data_all, data_tmp);
                 end
-
-                disp(['len: ', num2str(length(data_tmp.trialinfo))]); % debug
             end
         end
 
         data = data_all;
         save(fullfile(res_dir, [groups{gi}, '_', conditions{ci}, '.mat']), 'data', '-v7.3');
     end
+end
+
+% go/nogo
+for gi = 1:length(groups) % group index
+    % go trials
+    data_go = [];
+    data_nogo = [];
+    for pi = 1:12 % participant index
+        id = [groups{gi}, num2str(pi)];
+
+        disp(['--- id: ', id, ', start pre-processing ---']);
+        for si = 1:5 % segment index
+            id = [groups{gi}, num2str(pi), '-', num2str(si)];
+            
+            fname = fullfile(data_dir, [id, '.mat']);
+            if ~exist(fname, 'file')
+                continue;
+            end
+
+            disp('loading...');
+            load(fname); % include data
+
+            % extract data
+            cfg = [];
+            cfg.trials = find(data.trialinfo == 1 | data.trialinfo == 4);
+            tmp_go = ft_selectdata(cfg, data);
+            cfg = [];
+            cfg.trials = find(data.trialinfo == 2 | data.trialinfo == 3);
+            tmp_nogo = ft_selectdata(cfg, data);
+
+            % append
+            if isempty(data_go) & isempty(data_nogo)
+                data_go = tmp_go;
+                data_nogo = tmp_nogo;
+            else 
+                cfg = [];
+                data_go = ft_appenddata(cfg, data_go, tmp_go);
+                data_nogo = ft_appenddata(cfg, data_nogo, tmp_nogo);
+            end
+        end
+    end
+    save(fullfile(res_dir, [groups{gi}, '_go.mat']), 'data_go', '-v7.3');
+    save(fullfile(res_dir, [groups{gi}, '_nogo.mat']), 'data_nogo', '-v7.3');
 end
