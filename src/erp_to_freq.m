9c4abf3afbc1b47a4394a1f1c0e679e5da41469b
KaitoKurokochi <kaito.24.kurokochi.09@gmail.com>
2026-02-15 12:09:16 +0900

    Update: method

--- src/erp_to_freq.m
+++ src/erp_to_freq.m
@@ -9,23 +9,38 @@ if ~exist(res_dir, 'dir')
 end
 
 for gi = 1:length(groups)
-    for ci = 1:num_type
-        fname = fullfile(data_dir, [groups{gi}, '_', conditions{ci}, '.mat']);
-        
+    for ci = 1:length(conditions)
+    %     fname = fullfile(data_dir, [groups{gi}, '_', conditions{ci}, '.mat']);
+    % 
+    %     disp('loading...');
+    %     load(fname); % include data
+    % 
+    %     % to freq
+    %     cfg = [];
+    %     cfg.method      = 'mtmconvol';
+    %     cfg.taper       = 'hanning'; % ff
+    %     cfg.foi         = 1:1:100;
+    %     cfg.toi         = data.time{1}(1) : 0.05 : data.time{1}(end);
+    %     cfg.t_ftimwin   = ones(length(cfg.foi), 1) .* 0.5;
+    %     cfg.keeptrials   = 'yes';
+    %     freq = ft_freqanalysis(cfg, data);
+    % 
+    %     % save 
+    %     save(fullfile(res_dir, [groups{gi}, '_', conditions{ci}, '.mat']), 'freq', '-v7.3');
+
+        fname = fullfile(data_dir, [groups{gi}, '_', conditions{ci}, '.mat']); % incoude data
         disp('loading...');
         load(fname); % include data
 
-        % to freq
         cfg = [];
-        cfg.method      = 'mtmconvol';
-        cfg.taper       = 'hanning'; % ff
-        cfg.foi         = 1:1:100;
+        cfg.method      = 'wavelet';
+        cfg.output      = 'pow';
+        cfg.keeptrials  = 'yes';
+        cfg.foi         = logspace(log10(3),log10(90),30);
+        cfg.width       = logspace(log10(3),log10(30),30);
         cfg.toi         = data.time{1}(1) : 0.05 : data.time{1}(end);
-        cfg.t_ftimwin   = ones(length(cfg.foi), 1) .* 0.5;
-        cfg.keeptrials   = 'yes';
         freq = ft_freqanalysis(cfg, data);
 
-        % save 
         save(fullfile(res_dir, [groups{gi}, '_', conditions{ci}, '.mat']), 'freq', '-v7.3');
     end
 end
