5e9386e6990aec7dc033b9c35df2fd984f182d44
KaitoKurokochi <kaito.24.kurokochi.09@gmail.com>
2026-02-17 10:42:14 +0900

    Update: add fig

--- src/classification_by_label.m
+++ src/classification_by_label.m
@@ -9,7 +9,7 @@ if ~exist(res_dir, 'dir')
     mkdir(res_dir);
 end
 
-%% conditions
+% conditions
 for gi = 1:length(groups) % group index
     for ci = 1:length(conditions) % condition index
         data_all = [];
@@ -51,5 +51,70 @@ for gi = 1:length(groups) % group index
 
         data = data_all;
         save(fullfile(res_dir, [groups{gi}, '_', conditions{ci}, '.mat']), 'data', '-v7.3');
+    end
+end
+
+%% fig for each channel
+clear;
+config;
+
+data_dir = fullfile(prj_dir, 'result', 'erp_group_cond');
+res_dir = fullfile(prj_dir, 'result', 'fig_erp_group_cond_chan');
+if ~exist(res_dir, 'dir')
+    mkdir(res_dir);
+end
+
+for gi = 1:length(groups)
+    for ci = 1:length(conditions) 
+        disp('loading...');
+        load(fullfile(data_dir, [groups{gi}, '_', conditions{ci}, '.mat'])); % include data
+            
+        fig = figure('Position', [100, 100, 1600, 1200], 'Visible', 'off');
+        % multiplot
+        cfg = [];
+        cfg.layout        = 'easycapM11.lay';
+        cfg.linewidth     = 1.0;
+        ft_multiplotER(cfg, data);
+
+        saveas(fig, fullfile(res_dir, [groups{gi}, '_', conditions{ci}, '.jpg']));
+        close(fig);
+    end
+end
+
+%% fig topomap
+clear;
+config;
+
+data_dir = fullfile(prj_dir, 'result', 'erp_group_cond');
+res_dir = fullfile(prj_dir, 'result', 'fig_erp_group_cond_topo');
+if ~exist(res_dir, 'dir')
+    mkdir(res_dir);
+end
+ 
+for gi = 1:length(groups)
+    for ci = 1:length(conditions) 
+        disp('loading...');
+        load(fullfile(data_dir, [groups{gi}, '_', conditions{ci}, '.mat'])); % include data
+
+        % for each 50ms
+        for t = 0:0.05:0.55
+            t_strt = t;
+            t_end = t+0.05;
+
+            fig = figure('Position', [100, 100, 800, 600], 'Visible', 'off');
+
+            cfg = [];
+            cfg.xlim               = [t_strt, t_end];
+            cfg.zlim               = 'maxabs';
+            % cfg.baseline           = 'yes';
+            % cfg.baselinetype       = 'relative';
+            cfg.colorbar           = 'yes';
+            cfg.layout             = 'easycapM11.mat';
+            ft_topoplotER(cfg, data);
+
+            saveas(fig, fullfile(res_dir, [groups{gi}, '_', conditions{ci}, '_', ...
+                num2str(t_strt*1000), '_', num2str(t_end*1000), '.jpg']));
+            close(fig);
+        end
     end
 end
--- src/stat_erp_cbpt.m
+++ src/stat_erp_cbpt.m
@@ -13,7 +13,7 @@ end
 % neighbours
 load(fullfile(prj_dir, 'src', 'neighbours.mat'));
 
-%% 
+%% statistics - erp cbpt
 for ci = 1:length(conditions)
     % read data
     disp('--- loading ERP data ---');
@@ -43,4 +43,58 @@ for ci = 1:length(conditions)
 
     % save data
     save(fullfile(res_dir, [conditions{ci}, '.mat']), 'stat', '-v7.3');
+end
+
+%% figure result - time x amp for all channels
+clear;
+config;
+
+data_erp_dir = fullfile(prj_dir, 'result', 'erp_group_cond'); % set data dir
+data_stat_dir = fullfile(prj_dir, 'result', 'stat_erp_cbpt');
+res_dir = fullfile(prj_dir, 'result', 'fig_stat_erp_cbpt'); % set res dir
+if ~exist(res_dir, 'dir')
+    mkdir(res_dir);
+end
+
+for ci = 1:length(conditions)
+    % read ERP data
+    disp('--- loading ERP data ---');
+    % exp ERP
+    load(fullfile(data_erp_dir, ['exp_', conditions{ci}, '.mat'])); % include data
+    data_exp = data;
+    clear data;
+    % nov ERP
+    load(fullfile(data_erp_dir, ['nov_', conditions{ci}, '.mat'])); % include data
+    data_nov = data;
+    clear data;
+
+    % read stat data 
+    disp('--- loading stat data ---');
+    load(fullfile(data_stat_dir, [conditions{ci}, '.mat'])); % include stat
+
+    % calculate mean ERP
+    cfg = [];
+    cfg.latency = [0.0 0.6];
+    data_exp = ft_timelockanalysis(cfg, data_exp);
+    data_nov = ft_timelockanalysis(cfg, data_nov);
+
+    % add mask 
+    data_exp.mask = stat.mask;
+
+    % figure
+    cfg        = [];
+    cfg.layout = 'easycapM11.mat';
+    cfg.maskparameter = 'mask';
+    cfg.maskstyle     = 'box';
+    cfg.comment = ['Blue: ', conditions{ci}, ' (Exp), Red: Novice'];
+    cfg.showlabels = 'yes';
+    cfg.maskfacealpha = '0.2';
+
+    fig = figure('Position', [100, 100, 1600, 1200]);
+    ft_multiplotER(cfg, data_exp, data_nov);
+    title(['ERP CBPT - ', conditions{ci}]);
+
+    % save data and close figure 
+    saveas(fig, fullfile(res_dir, [conditions{ci}, '.jpg']));
+    % close(fig);
 end
