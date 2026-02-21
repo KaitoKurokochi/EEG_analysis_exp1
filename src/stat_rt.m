cdc0f02fc34aca42c16626968989046fbd13140e
KaitoKurokochi <kaito.24.kurokochi.09@gmail.com>
2026-02-21 15:58:13 +0900

    Update: for analysis

--- src/stat_rt.m
+++ src/stat_rt.m
@@ -1,6 +1,7 @@
 %% statistics of response time (go-condition)
-% compare between groups
+% compare between groups - indep T test
 
+clear;
 config;
 
 data_dir = fullfile(prj_dir, 'result', 'trialinfo'); % set data dir
@@ -36,8 +37,38 @@ stat = [];
 [stat.h, stat.p, stat.ci, stat.stats] = ttest2(exp.m_rt, nov.m_rt);
 
 % save data
-save(fullfile(res_dir, 'exp.mat'), 'exp');
-save(fullfile(res_dir, 'nov.mat'), 'nov');
+stat.exp = exp;
+stat.nov = nov;
 save(fullfile(res_dir, 'stat.mat'), 'stat');
 
+%% calculate mean
+m_rt_exp = mean(exp.m_rt);
+m_rt_nov = mean(nov.m_rt);
+
 %% figure statistics of RT
+clear;
+config;
+
+data_dir = fullfile(prj_dir, 'result', 'stat_rt'); % set data dir
+res_dir = fullfile(prj_dir, 'result', 'fig_stat_rt'); % set res dir
+if ~exist(res_dir, 'dir')
+    mkdir(res_dir);
+end
+
+% read data
+load(fullfile(data_dir, 'stat.mat'));
+
+% create table 
+rt_val = [stat.exp.m_rt'; stat.nov.m_rt'];
+group_label = [repmat({'Experimental'}, 12, 1); repmat({'Novice'}, 12, 1)];
+plot_tbl = table(group_label, rt_val, 'VariableNames', {'Group', 'RT'});
+
+fig = figure;
+b = boxchart(categorical(plot_tbl.Group), plot_tbl.RT);
+ylabel('Reaction Time (s)');
+title('Comparison of Reaction Time (Go-condition)');
+grid on;
+
+saveas(fig, fullfile(res_dir, 'boxplot.jpg'));
+close(fig);
+
