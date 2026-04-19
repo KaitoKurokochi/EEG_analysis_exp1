67aebc76c49359fd65f370c850ac98dcf4acb705
KaitoKurokochi <kaito.24.kurokochi.09@gmail.com>
2026-04-20 07:56:50 +0900

    Create: show diff of band topo

---
+++ src/supplemental/config.m
@@ -0,0 +1,6 @@
+prj_dir = 'C:\Users\kaito\workspace\2026_exp1\EEG_analysis_exp1';
+addpath("utils\");
+main_channels = {'Cz', 'Fz', 'Pz'};
+groups = {'nov', 'exp'};
+% conditions = {'ff', 'fs', 'sf', 'ss'};
+conditions = {'go', 'nogo'};
